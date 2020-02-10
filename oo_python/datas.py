1ebc103cc50d67b7b3749105fbd6bc6ad223abaf
Wilson L. Prosdocimo <wilson@pneufree.com.br>
2020-02-10 18:28:23 -0300

    Ajustado metodos

--- oo_python/datas.py
+++ oo_python/datas.py
@@ -1,7 +1,9 @@
 
 class Data:
     def __init__(self, dia, mes, ano):
-        data = [dia, mes, ano]
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
 
     def formatada(self):
-        print("{}/{}/{}".format(self.dia,self.mes,self.ano))
+        print("{}/{}/{}".format(self.dia, self.mes, self.ano))
