cfd1f4c97214fe34f8983a9fbadcdc9d2c4e938c
Wilson L. Prosdocimo <wilson@pneufree.com.br>
2020-02-11 18:08:52 -0300

    Adicionando a classe playlist com duck type

--- python3oo2/modelos.py
+++ python3oo2/modelos.py
@@ -41,13 +41,38 @@ class Serie(Programa):
         return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes'
 
 
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    def __len__(self):
+        return len(self._programas)
+
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.dar_likes()
-vingadores.dar_likes()
 atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('Todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
+
+vingadores.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
+atlanta.dar_likes()
+atlanta.dar_likes()
 atlanta.dar_likes()
 
-listinha = [atlanta, vingadores]
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-for programa in listinha:
+print(playlist_fim_de_semana.nome, len(playlist_fim_de_semana))
+
+for programa in playlist_fim_de_semana:
     print(programa)
