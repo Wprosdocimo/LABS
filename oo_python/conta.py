40a1b0d82f6dfb1e70187476c18659410c05c777
Wilson L. Prosdocimo <wilson@pneufree.com.br>
2020-02-11 07:26:28 -0300

    Adicionando setters e getters

--- oo_python/conta.py
+++ oo_python/conta.py
@@ -1,17 +1,28 @@
-
 class Conta:
     def __init__(self, numero, titular, saldo, limite=1000.00):
         print("Construindo objeto ... {}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("Saldo {} do titular {}".format(self.saldo,self.titular))
+        print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
 
     def depositar(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def sacar(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    def get_limite(self):
+        return self.__limite
+
+    def set_limite(self, limite):
+        self.__limite = limite
