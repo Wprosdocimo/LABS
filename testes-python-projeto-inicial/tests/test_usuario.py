09105fd60fecedaf067b801ef43229cb6d752e2e
Wilson L. Prosdocimo <wilson@pneufree.com.br>
2020-02-14 12:54:10 -0300

    Testes da classe usuário

--- testes-python-projeto-inicial/tests/test_usuario.py
+++ testes-python-projeto-inicial/tests/test_usuario.py
@@ -35,5 +35,4 @@ def test_deve_permitir_propor_lance_quando_o_valor_eh_igual_ao_valor_da_carteira
 
 def test_nao_deve_permitir_propor_lance_com_valor_maior_que_o_da_carteira(vini, leilao):
     with pytest.raises(LanceInvalido):
-
         vini.propoe_lance(leilao, 200.0)
