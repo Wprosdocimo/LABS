638fbf66eaddfbec85ea2f34c41d45bcd06741bf
Wilson L. Prosdocimo <wilson@pneufree.com.br>
2020-02-27 19:04:56 -0300

    Corrigindo o fluxo das activities

--- Agenda/app/src/main/java/com/example/agenda/ui/activity/FormularioAlunoActivity.java
+++ Agenda/app/src/main/java/com/example/agenda/ui/activity/FormularioAlunoActivity.java
@@ -2,6 +2,7 @@ package com.example.agenda.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +10,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.agenda.R;
+import com.example.agenda.dao.AlunoDAO;
 import com.example.agenda.model.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
@@ -17,6 +19,9 @@ public class FormularioAlunoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+
+        final AlunoDAO dao = new AlunoDAO();
+
         final EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
         final EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
         final EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
@@ -30,11 +35,9 @@ public class FormularioAlunoActivity extends AppCompatActivity {
                 String email = campoEmail.getText().toString();
 
                 Aluno alunoCriado = new Aluno(nome, telefone, email);
-                Toast.makeText(FormularioAlunoActivity.this,
-                        alunoCriado.getNome() + " - " +
-                                alunoCriado.getTelefone() + " - " +
-                                alunoCriado.getEmail(),
-                        Toast.LENGTH_SHORT).show();
+                dao.salva(alunoCriado);
+
+                finish();
             }
         });
     }
