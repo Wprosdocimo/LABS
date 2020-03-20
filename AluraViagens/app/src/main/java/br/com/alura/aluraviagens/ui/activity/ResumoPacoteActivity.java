2082d28d83d9b9d73ee1935e772bd45c7f63cf5e
Wilson Luiz Prosdocimo <wilson@wprosdocimo.com.br>
2020-03-20 13:56:45 -0300

    Adicionando tela de pagamento.

--- AluraViagens/app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoPacoteActivity.java
+++ AluraViagens/app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoPacoteActivity.java
@@ -2,6 +2,7 @@ package br.com.alura.aluraviagens.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -36,6 +37,9 @@ public class ResumoPacoteActivity extends AppCompatActivity {
         mostraDias(pacoteSaoPaulo);
         mostraPreco(pacoteSaoPaulo);
         mostraData(pacoteSaoPaulo);
+
+        Intent intent = new Intent(this, PagamentoActivity.class);
+        startActivity(intent);
     }
 
     private void mostraData(Pacote pacote) {
@@ -43,8 +47,6 @@ public class ResumoPacoteActivity extends AppCompatActivity {
         String dataFormatadaDaViagem = DataUtil.periodoEmTexto(pacote.getDias());
         data.setText(dataFormatadaDaViagem);
     }
-
-
 
     private void mostraPreco(Pacote pacote) {
         TextView preco = findViewById(R.id.resumo_pacote_valor);
