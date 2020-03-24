99c3b802f9a5fce1e1efe6d492effcef5ec1ef68
Wilson Luiz Prosdocimo <wilson@wprosdocimo.com.br>
2020-03-24 05:39:33 -0300

    Refatoração de código

--- Ceep/app/src/main/java/br/com/alura/ceep/ui/activity/FormularioNotaActivity.java
+++ Ceep/app/src/main/java/br/com/alura/ceep/ui/activity/FormularioNotaActivity.java
@@ -1,18 +1,19 @@
 package br.com.alura.ceep.ui.activity;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import br.com.alura.ceep.R;
-import br.com.alura.ceep.dao.NotaDAO;
 import br.com.alura.ceep.model.Nota;
+
+import static br.com.alura.ceep.ui.activity.NotaActivityContantes.CHAVE_NOTA;
+import static br.com.alura.ceep.ui.activity.NotaActivityContantes.CODIGO_RESULTADO_NOTA_CRIADA;
 
 public class FormularioNotaActivity extends AppCompatActivity {
 
@@ -32,16 +33,28 @@ public class FormularioNotaActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.menu_formulario_nota_ic_salva){
-            EditText titulo = findViewById(R.id.formulario_nota_titulo);
-            EditText descricao = findViewById(R.id.formulario_nota_descricao);
-            Nota notaCriada = new Nota(titulo.getText().toString(),
-                    descricao.getText().toString());
-            Intent resultadoInsercao = new Intent();
-            resultadoInsercao.putExtra("nota", notaCriada);
-            setResult(2,resultadoInsercao);
+        if (ehMenuSalvaNota(item)){
+            Nota notaCriada = criaNota();
+            retornaNota(notaCriada);
             finish();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void retornaNota(Nota nota) {
+        Intent resultadoInsercao = new Intent();
+        resultadoInsercao.putExtra(CHAVE_NOTA, nota);
+        setResult(CODIGO_RESULTADO_NOTA_CRIADA,resultadoInsercao);
+    }
+
+    private Nota criaNota() {
+        EditText titulo = findViewById(R.id.formulario_nota_titulo);
+        EditText descricao = findViewById(R.id.formulario_nota_descricao);
+        return new Nota(titulo.getText().toString(),
+                descricao.getText().toString());
+    }
+
+    private boolean ehMenuSalvaNota(@NonNull MenuItem item) {
+        return item.getItemId() == R.id.menu_formulario_nota_ic_salva;
     }
 }
