e6334db9e123256edf2396a3464717d6508c7962
Wilson Luiz Prosdocimo <wilson@wprosdocimo.com.br>
2020-03-25 17:18:24 -0300

    Adicionando persistencia dos dados com Room

---
+++ Agenda/app/src/main/java/com/example/agenda/database/AgendaDatabase.java
@@ -0,0 +1,25 @@
+package com.example.agenda.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.agenda.database.dao.RoomAlunoDAO;
+import com.example.agenda.model.Aluno;
+
+@Database(entities = {Aluno.class}, version = 2, exportSchema = false)
+public abstract class AgendaDatabase extends RoomDatabase {
+
+    private static final String NOME_BANCO_DE_DADOS = "agenda.db";
+
+    public abstract RoomAlunoDAO getRoomAlunoDAO();
+
+    public static AgendaDatabase getInstance(Context context){
+        return Room
+                .databaseBuilder(context, AgendaDatabase.class, NOME_BANCO_DE_DADOS)
+                .allowMainThreadQueries()
+                .build();
+    }
+}
--- Agenda/app/src/main/java/com/example/agenda/ui/activity/ListaAlunosActivity.java
+++ Agenda/app/src/main/java/com/example/agenda/ui/activity/ListaAlunosActivity.java
@@ -22,12 +22,12 @@ import static com.example.agenda.ui.activity.ConstantesActivities.CHAVE_ALUNO;
 public class ListaAlunosActivity extends AppCompatActivity {
 
     private static final String TITULO_APPBAR = "Lista de Alunos";
-
-    private final ListaAlunosView listaAlunosView = new ListaAlunosView(this);
+    private ListaAlunosView listaAlunosView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        listaAlunosView = new ListaAlunosView(this);
         setContentView(R.layout.activity_lista_alunos);
         setTitle(TITULO_APPBAR);
         configuraFabNovoAluno();
