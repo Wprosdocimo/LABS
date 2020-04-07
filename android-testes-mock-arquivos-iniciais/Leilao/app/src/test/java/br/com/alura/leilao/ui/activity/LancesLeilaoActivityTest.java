6a04ef1d2899bd9b88830a42c9513cbad09d89ed
Wilson Luiz Prosdocimo <wilson@wprosdocimo.com.br>
2020-04-07 05:36:59 -0300

    Modificando comportamentos com Answer

--- android-testes-mock-arquivos-iniciais/Leilao/app/src/test/java/br/com/alura/leilao/ui/activity/LancesLeilaoActivityTest.java
+++ android-testes-mock-arquivos-iniciais/Leilao/app/src/test/java/br/com/alura/leilao/ui/activity/LancesLeilaoActivityTest.java
@@ -4,16 +4,25 @@ import android.content.Context;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import br.com.alura.leilao.api.retrofit.client.LeilaoWebClient;
+import br.com.alura.leilao.api.retrofit.client.RespostaListener;
+import br.com.alura.leilao.model.Leilao;
 import br.com.alura.leilao.ui.recyclerview.adapter.ListaLeilaoAdapter;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LancesLeilaoActivityTest {
@@ -22,17 +31,29 @@ public class LancesLeilaoActivityTest {
     private Context context;
     @Spy
     private ListaLeilaoAdapter adapter = new ListaLeilaoAdapter(context);
+    @Mock
+    private LeilaoWebClient client;
 
     @Test
-    public void deve_AtualizarListadeLeiloes_QuandoBuscarLeiloesDaApi() throws InterruptedException {
+    public void deve_AtualizarListadeLeiloes_QuandoBuscarLeiloesDaApi() {
         ListaLeilaoActivity activity = new ListaLeilaoActivity();
         Mockito.doNothing().when(adapter).atualizaLista();
+        Mockito.doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) {
+                final RespostaListener<List<Leilao>> argument = invocation.getArgument(0);
+                argument.sucesso(new ArrayList<>(Arrays.asList(
+                        new Leilao("Computador"),
+                        new Leilao("Carro")
+                )));
+                return null;
+            }
+        }).when(client).todos(ArgumentMatchers.any(RespostaListener.class));
 
-        activity.buscaLeiloes(adapter);
-        Thread.sleep(2000);
+        activity.buscaLeiloes(adapter, client);
         final int quantidadeLeiloesDevolvida = adapter.getItemCount();
 
-        assertThat(quantidadeLeiloesDevolvida, is(3));
+        assertThat(quantidadeLeiloesDevolvida, is(2));
     }
 
 }
