f44fea9b74057341ba24d08c205213b0c1e09926
Wilson Luiz Prosdocimo <wilson@wprosdocimo.com.br>
2020-04-07 06:22:45 -0300

    Delegando responsabilidades

--- android-testes-mock-arquivos-iniciais/Leilao/app/src/test/java/br/com/alura/leilao/ui/AtualizadorDeLeiloesTest.java
+++ android-testes-mock-arquivos-iniciais/Leilao/app/src/test/java/br/com/alura/leilao/ui/AtualizadorDeLeiloesTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.stubbing.Answer;
@@ -21,7 +22,10 @@ import br.com.alura.leilao.ui.activity.ListaLeilaoActivity;
 import br.com.alura.leilao.ui.recyclerview.adapter.ListaLeilaoAdapter;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 
 
@@ -34,6 +38,8 @@ public class AtualizadorDeLeiloesTest {
     private LeilaoWebClient client;
     @Mock
     private Context context;
+    @Mock
+    private AtualizadorDeLeiloes.ErroCarregaLeiloesListener listener;
 
     @Test
     public void deve_AtualizarListadeLeiloes_QuandoBuscarLeiloesDaApi() {
@@ -48,14 +54,31 @@ public class AtualizadorDeLeiloesTest {
                 )));
                 return null;
             }
-        }).when(client).todos(ArgumentMatchers.any(RespostaListener.class));
+        }).when(client).todos(any(RespostaListener.class));
 
-        atualizador.buscaLeiloes(adapter, client, context);
-        verify(client).todos(ArgumentMatchers.any(RespostaListener.class));
+        atualizador.buscaLeiloes(adapter, client, listener);
+        verify(client).todos(any(RespostaListener.class));
         verify(adapter).atualiza(new ArrayList<>(Arrays.asList(
                 new Leilao("Computador"),
                 new Leilao("Carro")
         )));
+    }
+
+    @Test
+    public void deve_ApresentarMensagemDeFalha_QuandoFalharABuscaDeLeiLoes(){
+        AtualizadorDeLeiloes atualizador = new AtualizadorDeLeiloes();
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) {
+                final RespostaListener<List<Leilao>> argument = invocation.getArgument(0);
+                argument.falha(anyString());
+                return null;
+            }
+        }).when(client).todos(any(RespostaListener.class));
+
+        atualizador.buscaLeiloes(adapter, client, listener);
+
+        verify(listener).erroAoCarregar(anyString());
     }
 
 }
