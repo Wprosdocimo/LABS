1a5cd05742df90a6cad7f7b81e2bdc66717f2350
Wilson Luiz Prosdocimo <wilson@wprosdocimo.com.br>
2020-05-21 07:23:28 -0300

    Aula4

---
+++ financas-kotlin-parte1-master/FinancasK/app/src/main/java/br/com/alura/financask/extension/CalendarExtension.kt
@@ -0,0 +1,11 @@
+package br.com.alura.financask.extension
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+fun Calendar.formataParaBrasileiro() : String{
+    val formatoBrasileiro = "dd/mm/yyyy"
+    val format = SimpleDateFormat(formatoBrasileiro)
+    return format.format(this.time)
+}
--- financas-kotlin-parte1-master/FinancasK/app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
+++ financas-kotlin-parte1-master/FinancasK/app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import br.com.alura.financask.R
+import br.com.alura.financask.model.Tipo
 import br.com.alura.financask.model.Transacao
 import br.com.alura.financask.ui.adapter.ListaTansacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
@@ -15,12 +16,11 @@ class ListaTransacoesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
         val transacoes = listOf(
-            Transacao(BigDecimal(20.5),"Comida", Calendar.getInstance()),
-            Transacao(BigDecimal(100.0),"Economia", Calendar.getInstance())
+            Transacao(valor = BigDecimal( 20.5), tipo = Tipo.DESPESA ),
+            Transacao(valor = BigDecimal(100.0),categoria = "Economia", tipo = Tipo.RECEITA )
         )
         val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, transacoes)
 
-        lista_transacoes_listview.setAdapter(
-            ListaTansacoesAdapter(transacoes, this))
+        lista_transacoes_listview.adapter = ListaTansacoesAdapter(transacoes, this)
     }
 }
