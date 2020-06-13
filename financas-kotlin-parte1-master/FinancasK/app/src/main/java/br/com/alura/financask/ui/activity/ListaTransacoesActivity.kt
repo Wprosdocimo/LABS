3104e79e07ff67f55ae291bf074090e9b61aab87
Wilson Luiz Prosdocimo <wilson@wprosdocimo.com.br>
2020-06-13 07:09:31 -0300

    Aula3

--- financas-kotlin-parte1-master/FinancasK/app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
+++ financas-kotlin-parte1-master/FinancasK/app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
@@ -1,6 +1,7 @@
 package br.com.alura.financask.ui.activity
 
 import android.os.Bundle
+import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import br.com.alura.financask.R
@@ -10,11 +11,18 @@ import br.com.alura.financask.model.Transacao
 import br.com.alura.financask.ui.ResumoView
 import br.com.alura.financask.ui.adapter.ListaTansacoesAdapter
 import br.com.alura.financask.ui.dialog.AdicionaTransacaoDialog
+import br.com.alura.financask.ui.dialog.AlteraTransacaoDialog
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
 
     private val transacoes: MutableList<Transacao> = mutableListOf()
+    private val viewDaActivity: View  by lazy {
+        window.decorView
+    }
+    private val viewGroupDaActivity: ViewGroup by lazy {
+        viewDaActivity as ViewGroup
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,28 +45,56 @@ class ListaTransacoesActivity : AppCompatActivity() {
     }
 
     private fun chamaDiaologDeAdicao(tipo: Tipo) {
-        AdicionaTransacaoDialog(window.decorView as ViewGroup, this)
+        AdicionaTransacaoDialog(viewGroupDaActivity, this)
             .chama(tipo, object : TransacaoDelegate {
                 override fun delegate(transacao: Transacao) {
-                    atualizaTransacoes(transacao)
+                    adiciona(transacao)
                     lista_transacoes_adiciona_menu.close(true)
                 }
             })
     }
 
-    private fun atualizaTransacoes(transacao: Transacao) {
+    private fun adiciona(transacao: Transacao) {
         transacoes.add(transacao)
+        atualizaTransacoes()
+    }
+
+    private fun atualizaTransacoes() {
         configuraLista()
         configuraResumo()
     }
 
     private fun configuraResumo() {
-        val view = window.decorView
-        val resumoView = ResumoView(this, view, transacoes)
+        val resumoView = ResumoView(this, viewDaActivity, transacoes)
         resumoView.atualiza()
     }
 
     private fun configuraLista() {
-        lista_transacoes_listview.adapter = ListaTansacoesAdapter(transacoes, this)
+        val listaTansacoesAdapter = ListaTansacoesAdapter(transacoes, this)
+        with(lista_transacoes_listview){
+            adapter = listaTansacoesAdapter
+            setOnItemClickListener { _, _, position, _ ->
+                val transacao = transacoes[position]
+                chamaDialogDeAlteracao(transacao, position)
+            }
+        }
+    }
+
+    private fun chamaDialogDeAlteracao(
+        transacao: Transacao,
+        position: Int
+    ) {
+        AlteraTransacaoDialog(viewGroupDaActivity, this)
+            .chama(transacao, object : TransacaoDelegate {
+                override fun delegate(transacao: Transacao) {
+                    altera(transacao, position)
+                }
+
+            })
+    }
+
+    private fun altera(transacao: Transacao, position: Int) {
+        transacoes[position] = transacao
+        atualizaTransacoes()
     }
 }
---
+++ financas-kotlin-parte1-master/FinancasK/app/src/main/java/br/com/alura/financask/ui/dialog/AlteraTransacaoDialog.kt
@@ -0,0 +1,60 @@
+package br.com.alura.financask.ui.dialog
+
+import android.content.Context
+import android.view.ViewGroup
+import br.com.alura.financask.R
+import br.com.alura.financask.delegate.TransacaoDelegate
+import br.com.alura.financask.extension.formataParaBrasileiro
+import br.com.alura.financask.model.Tipo
+import br.com.alura.financask.model.Transacao
+
+class AlteraTransacaoDialog(
+    viewGroup: ViewGroup,
+    private val context: Context
+) : FormularioTransacaoDialog(context, viewGroup) {
+
+    fun chama(
+        transacao: Transacao,
+        transacaoDelegate: TransacaoDelegate
+    ) {
+        val tipo = transacao.tipo
+
+        super.chama(tipo, transacaoDelegate)
+        inicializaCampos(transacao)
+    }
+
+    private fun inicializaCampos(
+        transacao: Transacao
+    ) {
+        inicializaCampoValor(transacao)
+        inicializaCampoData(transacao)
+        inicializaCampoCaracteristica(transacao)
+    }
+
+    private fun inicializaCampoValor(transacao: Transacao) {
+        campoValor.setText(transacao.valor.toString())
+    }
+
+    private fun inicializaCampoData(transacao: Transacao) {
+        campoData.setText(transacao.data.formataParaBrasileiro())
+    }
+
+    private fun inicializaCampoCaracteristica(
+        transacao: Transacao
+    ) {
+        val tipo = transacao.tipo
+        val categoriasRetornadas = context.resources.getStringArray(categoriaPor(tipo))
+        val posicaoCategoria = categoriasRetornadas.indexOf(transacao.categoria)
+        campoCategoria.setSelection(posicaoCategoria, true)
+    }
+
+    override val tituloBotaoPositivo: String
+        get() = "Alterar"
+
+    override fun tituloPor(tipo: Tipo): Int    {
+        if (tipo == Tipo.RECEITA) {
+            return R.string.altera_receita
+        }
+        return R.string.altera_despesa
+    }
+}
