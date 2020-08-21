0f4ff009ba2bded994ce9e5c003402655963f7b8
Wilson Luiz Prosdocimo <wilson@wprosdocimo.com.br>
2020-08-21 09:47:41 -0300

    Alterando para projeto original do curso

---
+++ dotnet/Alura/ByteBank.Forum/src/ByteBank.Forum/Controllers/ContaController.cs
@@ -0,0 +1,38 @@
+﻿using ByteBank.Forum.Models;
+using ByteBank.Forum.ViewModels;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ByteBank.Forum.Controllers
+{
+    public class ContaController : Controller
+    {
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(ContaRegistrarViewModel modelo)
+        {
+            if (ModelState.IsValid)
+            {
+                var dbContext = new IdentityDbContext();
+                var novoUsuario = new IdentityUser();
+
+                novoUsuario.Email = modelo.Email;
+
+
+                // Podemos incluir o usuário
+                return RedirectToAction("Index", "Home");
+            }
+
+            //Alguma coisa deu errado
+            return View(modelo);
+        }
+    }
+}
