f334fb42f85b522159db3d40ba532cc05f6f2b75
Wilson Luiz Prosdocimo <wilson@wprosdocimo.com.br>
2020-09-16 06:34:11 -0300

    Adicionando novo projeto, do curso dotnet com mongodb.

---
+++ dotnet/Alura/Curso MongoDB 1/ExemplosMongoDB/ExemplosMongoDB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExemplosMongoDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
